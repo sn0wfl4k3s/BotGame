ce49ac288f594e31afbc9eaf3fbac5b1695881ea
Eduardo Júnior <ed.afram@gmail.com>
2019-03-01 20:09:00 -0300

    versão 1.0.0 pronta

--- BotGame/BotGame/BotGameBot.cs
+++ BotGame/BotGame/BotGameBot.cs
@@ -3,50 +3,132 @@
 
 using System.Threading;
 using System.Threading.Tasks;
+using BotGame.Models;
+using BotGame.Resource;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Bot.Schema;
 
 namespace BotGame
 {
-    /// <summary>
-    /// Represents a bot that processes incoming activities.
-    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
-    /// This is a Transient lifetime service. Transient lifetime services are created
-    /// each time they're requested. Objects that are expensive to construct, or have a lifetime
-    /// beyond a single turn, should be carefully managed.
-    /// For example, the <see cref="MemoryStorage"/> object and associated
-    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
-    /// </summary>
-    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
     public class BotGameBot : IBot
     {
-        /// <summary>
-        /// Initializes a new instance of the class.
-        /// </summary>                        
-        public BotGameBot()
+        private readonly DialogSet _dialogs;
+        private readonly BotGameAccessors _accessors;
+
+        public BotGameBot(BotGameAccessors accessors)
         {
+            _accessors = accessors ?? throw new System.ArgumentException(nameof(accessors));
+            _dialogs = new DialogSet(accessors.ConversationDialogState);
+
+            _dialogs
+                .Add(new TextPrompt(PromptType.Text))
+                .Add(new NumberPrompt<int>(PromptType.Number))
+                .Add(new ChoicePrompt(PromptType.Choice));
+
+            var beginGame = new WaterfallStep[]
+            {
+                SolicitarNome,
+                PerguntarSeQuerJogar,
+                DecidirOQueFazer
+            };
+            var playGame = new WaterfallStep[]
+            {
+                SolicitarNumeroAoUsuario,
+                VerificarSeAcertou
+            };
+
+            _dialogs.Add(new WaterfallDialog(WaterFallDialogID.BeginProgram, beginGame));
+            _dialogs.Add(new WaterfallDialog(WaterFallDialogID.StartGame, playGame));
         }
 
-        /// <summary>
-        /// Every conversation turn calls this method.
-        /// </summary>
-        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
-        /// for processing this conversation turn. </param>
-        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
-        /// or threads to receive notice of cancellation.</param>
-        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
-        /// <seealso cref="BotStateSet"/>
-        /// <seealso cref="ConversationState"/>
+        #region Começo do Programa
+        private async Task<DialogTurnResult> SolicitarNome(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(PromptType.Text, new PromptOptions
+            {
+                Prompt = MessageFactory.Text(ConversationMessages.askName)
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> PerguntarSeQuerJogar(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            Usuario usuario = await _accessors.Usuario.GetAsync(stepContext.Context, () => new Usuario(), cancellationToken);
+            usuario.Nome = (string)stepContext.Result;
+            return await stepContext.PromptAsync(PromptType.Choice, new PromptOptions
+            {
+                Prompt = MessageFactory.Text(ConversationMessages.askPlay(usuario.Nome)),
+                Choices = ChoiceFactory.ToChoices(ConversationInfoData.SelectOption)
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> DecidirOQueFazer(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            string resposta_do_usuario = ((FoundChoice)stepContext.Result).Value;
+            if (resposta_do_usuario.Equals(ConversationInfoData.SelectOption[0]))
+            {
+                Game game = await _accessors.Game.GetAsync(stepContext.Context, () => new Game(), cancellationToken);
+                game.Gerar_Numero_Aleatorio();
+                await stepContext.Context.SendActivityAsync(ConversationMessages.letsPlay);
+                return await stepContext.ReplaceDialogAsync(WaterFallDialogID.StartGame);
+            }
+            await stepContext.Context.SendActivityAsync(ConversationMessages.dontLetsPlay);
+            return await stepContext.EndDialogAsync();
+        }
+        #endregion
+
+        #region Durante o Jogo
+        private async Task<DialogTurnResult> SolicitarNumeroAoUsuario(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(PromptType.Number, new PromptOptions
+            {
+                Prompt = MessageFactory.Text(ConversationMessages.askWhatNumber)
+            }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> VerificarSeAcertou(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            int numero_sugerido_pelo_usuario = (int)stepContext.Result;
+            Usuario usuario = await _accessors.Usuario.GetAsync(stepContext.Context);
+            Game game = await _accessors.Game.GetAsync(stepContext.Context);
+            string resultado = game.Verificar_Se_Chegou_Perto(numero_sugerido_pelo_usuario);
+            if (resultado.Equals("acertou"))
+            {
+                await stepContext.Context.SendActivityAsync(ConversationMessages.win);
+                return await stepContext.EndDialogAsync();
+            }
+
+            await stepContext.Context.SendActivityAsync(ConversationMessages.tryAgain(resultado, usuario.Nome));
+            return await stepContext.ReplaceDialogAsync(WaterFallDialogID.StartGame);
+        }
+        #endregion
+
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
-            // Handle Message activity type, which is the main activity type for shown within a conversational interface
-            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
-            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
             if (turnContext.Activity.Type == ActivityTypes.Message)
             {
-                // Echo back to the user whatever they typed.             
-                await turnContext.SendActivityAsync("Hello World", cancellationToken: cancellationToken);
+                var dialogContext = await _dialogs.CreateContextAsync(turnContext, cancellationToken);
+                var results = await dialogContext.ContinueDialogAsync(cancellationToken);
+
+                if (results.Status == DialogTurnStatus.Empty)
+                {
+                    await dialogContext.BeginDialogAsync(WaterFallDialogID.BeginProgram, null, cancellationToken);
+                }
             }
+            else if (turnContext.Activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                foreach (var member in turnContext.Activity.MembersAdded)
+                {
+                    if (member.Id == turnContext.Activity.Recipient.Id)
+                    {
+                        await turnContext.SendActivityAsync(MessageFactory.Text(ConversationMessages.welcome));
+                    }
+                }
+            }
+
+            await _accessors.ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await _accessors.UserState.SaveChangesAsync(turnContext, false, cancellationToken);
         }
     }
 }
---
+++ BotGame/BotGame/Resource/Constants.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace BotGame.Resource
+{
+    public class PromptType
+    {
+        public const string Text = "texto";
+        public const string Number = "number";
+        public const string Choice = "choice";
+    }
+
+    public class WaterFallDialogID
+    {
+        public const string BeginProgram = "beginGame";
+        public const string StartGame = "startGame";
+    }
+
+    public class ConversationMessages
+    {
+        public const string welcome = "Bem-Vindo";
+        public static string askName = $"Olá, eu me chamo {ConversationInfoData.NameBot}, Qual o seu nome?";
+        public static string askPlay (string name){return $"Então {name}, Gostaria de tentar advinhar em que número estou pensando?";}
+        public static string tryAgain (string temperatura, string nome){return $"Tá {temperatura} {nome}, Tente novamente";}
+        public const string letsPlay = "Certo, então vamos jogar! =D";
+        public const string dontLetsPlay = "Ok, tudo bem então.. =/";
+        public const string askWhatNumber = "Qual número estou pensando?";
+        public const string win = "Parabéns, você acertou! =D";
+    }
+
+    public class ConversationInfoData
+    {
+        public const string NameBot = "RicarBot";
+        public static List<string> SelectOption = new List<string> { "Sim", "Não" };
+    }
+}
